e9584ce29bb746bb3e82cb89251cb0f4d6f489f0
PR Lai <plai0114@gmail.com>
2022-10-25 17:11:20 +0800

    add read file

--- products.py
+++ products.py
@@ -1,6 +1,15 @@
 # 記帳程式
-# 二維清單
-products = [] # products是大清單
+# 讀取檔案
+products = []
+with open('products.csv', 'r', encoding='utf-8') as f:
+	for line in f:
+		if '商品,價格' in line:
+			continue
+		name, price = line.strip().split(',')
+		products.append([name, price])
+print(products)
+
+# 讓使用者輸入
 while True:
 	name = input('請輸入商品名稱: ')
 	if name == 'q':
@@ -9,17 +18,12 @@ while True:
 	products.append([name, price]) # 大清單裡面裝著小清單
 print(products)
 
-# for 迴圈印出內容
+# 印出所有購買記錄
 for p in products:
 	print(p[0], '的價格是', p[1])
 
-# 寫入txt檔案
-with open('products.txt', 'w') as f:
-	for p in products:
-		f.write(p[0] + ',' + p[1] + '\n')
-
 # 寫入csv檔案
-with open('products.csv', 'w', encoding = 'utf-8') as f:
-	f.write('商品,價格\n')
+with open('products.csv', 'w', encoding='utf-8') as f:
+	# f.write('商品,價格\n')
 	for p in products:
 		f.write(p[0] + ',' + p[1] + '\n')
